c6833d36da36600e60c220e7821badc8a44efdf1
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-03 23:43:38 -0400

    Increased auth modularity

---
+++ client/src/modules/login.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import URL from '../AJAX_URL.js';
+
+// ACTION TYPES
+export const LOGIN_REQUESTED = 'LOGIN_REQUESTED'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+// export const LOGOUT_REQUESTED = 'LOGOUT_REQUESTED'
+
+// ACTION CREATORS (ADD SET TMEOUTS HERE)
+export const login = (username, password, callback) => dispatch => {
+  dispatch({ type: LOGIN_REQUESTED })
+  axios
+    .put(`${URL}/api/auth/login`, { username, password })
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res
+      })
+      if (callback && typeof callback === 'function') {
+        callback(res.token)
+      }
+    })
+    .catch(err =>
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.message
+      })
+    )
+}
+
+// export function logout() {
+//   localStorage.clear()
+//   return { type: LOGOUT_REQUESTED }
+// }
+
+// REDUCERS
+const initState = {
+  loggingIn: false,
+  currentUser: null,
+  status: '',
+  error: null,
+}
+
+export function loginReducer(state = initState, action) {
+  switch (action.type) {
+    case LOGIN_REQUESTED:
+      return { ...state, loggingIn: true, status: '', token: '' }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        error: null,
+        currentUser: action.payload.user,
+        status: action.payload.message,
+      }
+    case LOGIN_FAIL:
+      return { ...state, loggingIn: false, error: action.payload }
+    // case LOGOUT_REQUESTED:
+    //   return {
+    //     loggingIn: false,
+    //     currentUser: null,
+    //     error: null,
+    //   }
+    default:
+      return state
+  }
+}
--- client/src/modules/register.js
+++ client/src/modules/register.js
@@ -1,20 +1,12 @@
+import axios from 'axios';
+import URL from '../AJAX_URL.js';
+
 // ACTION TYPES
-export const LOGIN_REQUESTED = 'LOGIN_REQUESTED'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAIL = 'LOGIN_FAIL'
-
-export const SET_USER = 'SET_USER'
-export const SET_AUTH = 'SET_AUTH'
-
-export const LOGOUT_REQUESTED = 'LOGOUT_REQUESTED'
-
 export const REGISTRATION_REQUESTED = 'REGISTRATION_REQUESTED'
 export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS'
 export const REGISTRATION_FAIL = 'REGISTRATION_FAIL'
 
-// ACTION CREATORS (ADD SET TMEOUTS HERE)
-const URL = 'http://localhost:5000'
-
+// ACTION CREATORS (ADD TMEOUTS HERE)
 export const register = (username, department, password) => dispatch => {
   dispatch({ type: REGISTRATION_REQUESTED })
   axios
@@ -33,40 +25,14 @@ export const register = (username, department, password) => dispatch => {
     })
 }
 
-export const login = (username, password, callback) => dispatch => {
-  dispatch({ type: LOGIN_REQUESTED })
-  axios
-    .put(`${URL}/api/auth/login`, { username, password })
-    .then(res => {
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: res
-      })
-      if (callback && typeof callback === 'function') {
-        callback(res.token)
-      }
-    })
-    .catch(err =>
-      dispatch({
-        type: LOGIN_FAIL,
-        payload: err.message
-      })
-    )
-}
-
-// export function logout() {
-//   localStorage.clear()
-//   return { type: LOGOUT_REQUESTED }
-// }
-
 // REDUCERS
-const initStateRegistration = {
+const initState = {
   registering: false,
   status: '',
   error: null
 }
 
-export function registrationReducer(state = initStateRegistration, action) {
+export function registrationReducer(state = initState, action) {
   switch (action.type) {
     case REGISTRATION_REQUESTED:
       return { ...state, registering: true, status: '' }
@@ -82,38 +48,6 @@ export function registrationReducer(state = initStateRegistration, action) {
         registering: false,
         error: action.payload
       }
-    default:
-      return state
-  }
-}
-
-let initStateLogin = {
-  loggingIn: false,
-  currentUser: null,
-  status: '',
-  error: null,
-}
-
-export function loginReducer(state = initStateLogin, action) {
-  switch (action.type) {
-    case LOGIN_REQUESTED:
-      return { ...state, loggingIn: true, status: '', token: '' }
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        loggingIn: false,
-        error: null,
-        currentUser: action.payload.user,
-        status: action.payload.message,
-      }
-    case LOGIN_FAIL:
-      return { ...state, loggingIn: false, error: action.payload }
-    // case LOGOUT_REQUESTED:
-    //   return {
-    //     loggingIn: false,
-    //     currentUser: null,
-    //     error: null,
-    //   }
     default:
       return state
   }
