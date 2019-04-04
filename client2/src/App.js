114a1e098d9553e4639333b315dd846aeca07b76
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-04 16:15:55 -0400

    Wired up Home and Login components

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+// import logo from './logo.svg';
 import './App.css';
+
+import Login from './components/Login.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
+        
+        <>
+          <Route exact path='/' component = {Home} />
+          <Route path='/login' component = {Login} />
+        </>
       </div>
     );
   }
 }
+
+const Home = props => (
+  <div>Hello world</div>
+)
 
 export default App;
---
+++ client2/src/components/Login.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import axios from 'axios'
+
+// import { login } from '../../modules/login.js'
+
+// INLINE COMPONENTS (FOR TOGGLING)
+const loginError = toggleFlag => {
+  return {
+    color: "red",
+    fontSize: "0.9rem",
+    display: toggleFlag ? "block" : "none",
+    marginTop: "25px"
+  };
+};
+
+export class LoginForm extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>
+          Login
+        </h2>
+
+        <label htmlFor="username">Username</label>
+        <input
+          onChange={this.handleChange}
+          name="username"
+          type="text"
+          value={this.state.username}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={this.handleChange}
+          name="password"
+          type="password"
+          value={this.state.password}
+          className="last-input"
+        />
+
+        <div>
+          {this.props.loggingIn ? (
+            <h3>Logging in.... please wait...</h3>
+          ) : (
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          )}
+
+          {/* <button onClick={this.handleSignUp} className="register-btn">
+            Register
+          </button> */}
+        </div>
+
+        <label style={loginError(this.props.error)}>
+          Error: {this.props.error}. Please try again.
+        </label>
+      </form>
+    );
+  }
+  
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  // WITH REDUX
+  // handleSubmit = event => {
+  //   event.preventDefault();
+  //   this.props.login(this.state.username, this.state.password, () =>
+  //     this.props.history.push("/")
+  //   );
+
+  //   this.setState({
+  //     username: "",
+  //     password: ""
+  //   });
+  // };
+
+  // W/O REDUX
+  handleSubmit = event => {
+    event.preventDefault();
+    axios.put('http://localhost:5000/api/auth/login', this.state)
+      .then(res => {
+        console.log('LOGIN RESPONSE', res.data)
+        localStorage.setItem('token', res.data.token);
+      })
+      .catch(error => {
+        console.log('ERROR', error)
+      })
+  }
+
+  // handleSignUp = event => {
+  //   event.preventDefault();
+  //   this.props.history.push("/signup");
+  // };
+}
+
+// const mapStateToProps = state => ({
+//   loggingIn: state.login.loggingIn,
+//   currentUser: state.login.currentUser,
+//   status: state.login.status,
+//   error: state.login.error
+// })
+
+// export default connect(
+//   mapStateToProps,
+//   { login }
+// )(LoginForm);
+
+export default LoginForm;
